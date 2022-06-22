a6325b38ac3db14e22e08c1ed32507f6b24bb3ea
mrima <mrima@bullseye>
2022-06-22 21:06:46 +0300

    format promise consumption

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -7,15 +7,15 @@ client.on('ready', () => console.log('Redis client connected to the server'));
 client.connect();
 
 const setNewSchool = (schoolName, value) => {
-  const res = client.set(schoolName, value);
-  res.then((reply) => {
-    console.log(`Reply: ${reply}`);
-  }).catch(err => console.log(err));
+  client.set(schoolName, value)
+    .then((reply) => console.log(`Reply: ${reply}`))
+    .catch(err => console.log(err));
 };
 
 const displaySchoolValue = (schoolName) => {
-  const school = client.get(schoolName);
-  school.then(data => console.log(data)).catch(err => console.log(err));
+  client.get(schoolName)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 };
 
 displaySchoolValue('Holberton');
